86058308f7b4c396a4f1a11a172a64ef22a35da0
KikoBuckets <145606152+KikoBuckets@users.noreply.github.com>
2023-09-27 11:53:01 -0600

    Create condicionales01.py

---
+++ condicionales01.py
@@ -0,0 +1,8 @@
+num=float(input("inserte un número"))
+if num<0 :
+  print("el número es negativo")
+elif num>0 :
+  print("el número es positivo")
+elif num == 0 : 
+  print("el número es cero")
+  
