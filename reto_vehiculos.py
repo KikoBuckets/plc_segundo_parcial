7a4603afa61358075faeb5726f07fc7c8ec5fc46
KikoBuckets <145606152+KikoBuckets@users.noreply.github.com>
2023-10-17 08:20:59 -0600

    reto_vehiculos.py

--- reto_vehiculos.py
+++ reto_vehiculos.py
@@ -1,3 +1,4 @@
+
 numero_ruedas = int(input("Número de ruedas: "))
 tipo_combustible = input("Tipo de combustible (gasolina, diésel, eléctrico): ")
 emisiones_co2 = float(input("Emisiones de CO2 en g/km: "))
@@ -8,5 +9,8 @@ elif numero_ruedas > 4 and tipo_combustible == "gasolina" and emisiones_co2 > 15
     clasificacion = "Vehículo grande y contaminante"
 else:
     clasificacion = tipo_combustible
+
+print(f"El vehículo se clasifica como: {clasificacion}")
+
 
   
