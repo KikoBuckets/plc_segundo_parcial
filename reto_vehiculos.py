63d8b6c6442cf062c712c7dd6a905e25659c9119
KikoBuckets <145606152+KikoBuckets@users.noreply.github.com>
2023-10-17 08:18:36 -0600

    Update reto_vehiculos.py

--- reto_vehiculos.py
+++ reto_vehiculos.py
@@ -1,22 +1,12 @@
-def clasificar_vehiculo():
-  numero_ruedas = int(input("Número de ruedas del vehículo: "))
-  tipo_combustible = input("Tipo de combustible (diesel, eléctrico o gasolina): ")
-  emision_co2 =  float(input("tipo de emisión (g/km): "))
+numero_ruedas = int(input("Número de ruedas: "))
+tipo_combustible = input("Tipo de combustible (gasolina, diésel, eléctrico): ")
+emisiones_co2 = float(input("Emisiones de CO2 en g/km: "))
 
-  if numero_ruedas == 2:
-      if tipo_combustible == "gasolina" and emision_co2 < 150:
-          print("Es una moto contaminante")
-      elif tipo_combustible == "eléctrico":
-          print("Es una moto eléctrica")
-      else:
-          print("Es una moto convencional")
-        
-  elif numero_ruedas == 4:
-if tipo_combustible == "eléctrico":
-  print("Es un coche eléctrico")
+if numero_ruedas == 4 and tipo_combustible == "eléctrico":
+    clasificacion = "Vehículo eléctrico"
+elif numero_ruedas > 4 and tipo_combustible == "gasolina" and emisiones_co2 > 150:
+    clasificacion = "Vehículo grande y contaminante"
 else:
- print("Es un vehículo convencional")
-clasificar_vehiculo()
-
+    clasificacion = tipo_combustible
 
   
